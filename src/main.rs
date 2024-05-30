dde09155bc0f645dab48abdb1ca50e9d961bccd9
Crystal Keck <meaneea@gmail.com>
2024-05-30 04:39:37 +0000

    Basic blink example

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,25 @@
 #![no_main]
 
 use esp_backtrace as _;
-use esp_hal::{clock::ClockControl, delay::Delay, peripherals::Peripherals, prelude::*};
+use esp_hal::{clock::ClockControl, delay::Delay, gpio::IO, peripherals::Peripherals, prelude::*};
 
 #[entry]
 fn main() -> ! {
     let peripherals = Peripherals::take();
     let system = peripherals.SYSTEM.split();
 
+    esp_println::logger::init_logger_from_env();
+    log::info!("Hello world!");
+
     let clocks = ClockControl::max(system.clock_control).freeze();
     let delay = Delay::new(&clocks);
 
-    esp_println::logger::init_logger_from_env();
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let mut led = io.pins.gpio2.into_push_pull_output();
 
     loop {
-        log::info!("Hello world!");
+        led.toggle();
+        log::info!("Blink!");
         delay.delay(500.millis());
     }
 }
